972abd5d450dd1c1fc5734bd1e4877c7bb66cdf6
vzaporozky <zaporozhskiy2002@gmail.com>
2024-11-28 22:52:10 +0200

    delete owner

--- src/Cat/cat.schema.ts
+++ src/Cat/cat.schema.ts
@@ -2,8 +2,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
 
-import { Owner } from '../Owner/owner.schema';
-
 export type CatDocument = HydratedDocument<Cat>;
 
 @Schema()
@@ -13,9 +11,6 @@ export class Cat {
 
   @Prop()
   age: number;
-
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Owner' }] })
-  owner: Owner[];
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
